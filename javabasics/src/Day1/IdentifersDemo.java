f10841f56bcabb9d4fb9f157b810af6f9aadf46f
Suseendran <Suseendran@LAPTOP-NBD0IOJ4>
2024-07-01 11:12:23 +0530

    This is My Day 1 Programs

---
+++ javabasics/src/Day1/IdentifersDemo.java
@@ -0,0 +1,16 @@
+package Day1;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number2=13;
+		System.out.println("value of the number varaible is:"+number2);
+		int $number=20;
+		System.out.println("value of the number varaible is:"+$number);
+		String studentName="Aniket";
+		System.out.println("value is:"+studentName);
+
+	}
+
+}
