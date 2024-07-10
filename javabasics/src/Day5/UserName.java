3d067ce0892256058a2f574dbb6196b51f26b9be
Suseendran <Suseendran@LAPTOP-NBD0IOJ4>
2024-07-10 13:40:32 +0530

    this is My Day5 Programs

---
+++ javabasics/src/Day5/UserName.java
@@ -0,0 +1,16 @@
+package Day5;
+
+import java.util.Scanner;
+
+public class UserName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input= new Scanner(System.in);
+		System.out.println("Enter your name:");
+		String n=input.nextLine();
+		System.out.println("Hello "+n);
+
+	}
+
+}
